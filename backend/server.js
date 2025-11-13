ac9db480d3742805ea3a2596987149b44bd81569
San <warcommandar455@gmail.com>
2025-11-13 10:18:23 +0300

    deleting frontend

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
 });
+
+
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  
+  socket.on("message", (data)=> {
+    console.log("message",data);
+    
+  })
+  socket.on("list", (data)=>{
+    io.emit("receieve_message", data)
+  })
 });
 
 server.listen(3000, () => {
